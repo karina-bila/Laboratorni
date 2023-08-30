42acae33ba34ddf023514517751cc2e2302a62e8
karina-bila <karinaslusar246@gmail.com>
2023-08-30 22:29:15 +0300

    Лабораторна 1.1

---
+++ Laboratorni/Lab.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double f, a = -1, b = -4, c = 6.3;
+
+    f = ((sqrt(a / b - a) / (pow(b, 2) + a * b)) - fabs(b + 2));
+    cout << "Результат f=" << f;
+
+    return 0;
+}
