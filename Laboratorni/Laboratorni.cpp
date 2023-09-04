8289bf46616d6ff6dd76ddbbfc0bf6c1e2fffd40
karina-bila <karinaslusar246@gmail.com>
2023-09-04 22:44:50 +0300

    лабораторна 2.1

--- Laboratorni/Laboratorni.cpp
+++ Laboratorni/Laboratorni.cpp
@@ -1,20 +1,30 @@
-﻿// Laboratorni.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+#include <windows.h>
 
-#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int x;
+	cout << "Введіть число x: ";
+	cin >> x;
+
+	double y;
+
+	if (x < 0) {
+		y = pow(sin(x), 2);
+	}
+	else if (x >= 0) {
+		y = log(x + 1);
+	}
+
+	cout << "\nФункція y=" << y;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
