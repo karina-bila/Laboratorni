4c812035f7f33daaf1bec75164ba86bb7734d019
karina-bila <karinaslusar246@gmail.com>
2023-09-04 23:15:06 +0300

    лабораторна 2.2

--- Laboratorni/Laboratorni.cpp
+++ Laboratorni/Laboratorni.cpp
@@ -1,20 +1,45 @@
-﻿// Laboratorni.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
 
-#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int number;
+	cout << "Введіть номер маршруту трамвая: ";
+	cin >> number;
+
+	string text = "\nТрамвай рухається по маршруту: ";
+
+	switch (number) {
+	case 1:
+		cout << text << "Муніципальний ринок - Електромережа";
+		break;
+
+	case 2:
+		cout << text << "Барське шосе - Вишенька";
+		break;
+
+	case 3:
+		cout << text << "Електромережа - Вишенька";
+		break;
+
+	case 4:
+		cout << text << "Муніципальний ринок - Барське шосе";
+		break;
+
+	case 5:
+		cout << text << "Електромережа - Барське шосе";
+		break;
+
+	case 6:
+		cout << text << "Вишенька - Муніципальний ринок";
+		break;
+
+	default: cout << "Такого маршруту не існує!";
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
