28e4309fa9d55b044de58ccd03fa01030929e82a
karina-bila <karinaslusar246@gmail.com>
2023-09-11 21:03:49 +0300

    Лабораторна 4-5.2

--- Laboratorni/Laboratorni.cpp
+++ Laboratorni/Laboratorni.cpp
@@ -1,31 +1,26 @@
 ﻿#include <iostream>
 #include <cmath>
-#include <string>
 #include <windows.h>
-
+#include <string>
 using namespace std;
 
-int main()
-{
+int main() {
+
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	double a, b, x, y, h;
-	int k;
-	a = 0;
-	b = 5;
-	cout << "Введіть кількість точок k=";
+	double a = 0.0; // Початок відрізку
+	double b = 5.0; // Кінець відрізку
+	int k; // Кількість точок
+	cout << "Введіть натуральне число: k=";
 	cin >> k;
-	h = (b - a) / k;
-	x = a;
-	cout << "x           y" << endl;
-	while (x <= b)
-	{
-		y = sin(pow(x, 2) + 4);
-		x += h; //x=x+h
+	double h = (b - a) / k;
 
-		cout << x << "     " << y << endl;
-
+	for (int i = 0; i <= k; i++) {
+		double xi = a + i * h;
+		double y = sin(pow(xi, 2) + 4);
+		cout << "x[" << i << "] = " << xi << ", y[" << i << "] = " << y << endl;
 	}
 
+	return 0;
 }
