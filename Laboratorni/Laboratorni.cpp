0cca80090da838f99dc4edf9af03ae3f6e55f25c
karina-bila <karinaslusar246@gmail.com>
2023-10-02 23:58:59 +0300

    Лабораторна 9-10.2

--- Laboratorni/Laboratorni.cpp
+++ Laboratorni/Laboratorni.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 #include <string>
-#include <windows.h>
+#include <algorithm>
 
 //Створити одновимірний масив з 10 слів різних. Скласти програму:
 // - впорядкувати масив в алфавітному порядку;
@@ -10,7 +10,33 @@ using namespace std;
 
 int main()
 {
+	cout << "=================\n";
 
+	string list[10] = {
+		"apple",
+		"elephant",
+		"guitar",
+		"sunshine",
+		"bicycle",
+		"ocean",
+		"happiness",
+		"mountain",
+		"bookshelf",
+		"universe",
+	};
 
+	int size = sizeof(list) / sizeof(list[0]);
+
+	for (int i = 0; i < size; ++i) {
+		cout << i + 1 << " " << list[i] << "\n";
+	}
+
+	sort(list, list + size);
+
+	cout << "=================\n";
+
+	for (int i = 0; i < size; ++i) {
+		cout << i + 1 << " " << list[i] << " - " << list[i].length() << "\n";
+	}
 }
 
