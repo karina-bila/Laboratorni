87a36e1ebdcf74a08b260bb85496955bedabda5d
karina-bila <karinaslusar246@gmail.com>
2023-10-02 23:39:04 +0300

    Лабораторна 9-10.1

--- Laboratorni/Laboratorni.cpp
+++ Laboratorni/Laboratorni.cpp
@@ -8,7 +8,18 @@ using namespace std;
 
 int main()
 {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 
+	string word = "астрономія";
 
+	string word1 = word.substr(0, 1) + word.substr(2, 1) + word.substr(6, 2);
+	string word2 = word.substr(0, 8);
+	string word3 = word.substr(1, 2) + word.substr(4, 1) + word.substr(3, 2) + word.substr(5, 1) + word.substr(0, 1);
 
+	cout << "Іменник 1: " << word1 << ", довжина: " << word1.length() << "\n";
+	cout << "Іменник 2: " << word2 << ", довжина: " << word2.length() << "\n";
+	cout << "Іменник 3: " << word3 << ", довжина: " << word3.length() << "\n";
+
+	return 0;
 }
