852aa747fcd86ac54e4bb492aae7a6feb81df549
karina-bila <karinaslusar246@gmail.com>
2023-10-05 22:58:33 +0300

    Лабораторна 11.2

--- Laboratorni/Laboratorni.cpp
+++ Laboratorni/Laboratorni.cpp
@@ -1,14 +1,30 @@
 ﻿#include <iostream>
 #include <string>
-#include <windows.h>
-
-//Знайти суму елементів двох матриць розміром 3×3.
+#include <Windows.h>
 
 using namespace std;
+
+double findSum(int x, int k)
+{
+	double summ = 0;
+	for (int i = k; i <= x; i++)
+	{
+		summ += (pow(2, k) * sin(x + k)) / (pow(x + 1, k));
+	}
+	return summ;
+}
 
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
+
+	int x = 7, k = 1;
+
+	cout << "x = " << x << endl;
+	cout << "k = " << k << endl;
+	double summ = findSum(x, k);
+
+	cout << "Сумма = " << summ << endl;
 
 }
